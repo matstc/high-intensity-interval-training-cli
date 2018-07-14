92232c8d459caa4aec897b255eb7c9ef1d27a092
Matt Tanguay-Carel <matthieutc@gmail.com>
2018-07-14 19:51:46 +0200

    builds basic hiit training

---
+++ README.md
@@ -0,0 +1,36 @@
+# High-intensity Interval Training CLI
+
+Write down your training steps in workout.txt, one step per line.
+
+Then run:
+
+    ./workout.rb
+
+
+# Example Output
+
+```
+30 seconds of work
+8 seconds of rest
+The workout will last 2.5 minutes
+
+Get ready for SQUATS
+GO!
+..........DONE
+Get ready for CRUNCHES (variations: crossing, legs stretched, leg raises)
+GO!
+..........DONE
+Get ready for PUSH-UPS (variations: wide, diamond)
+GO!
+..........DONE
+Get ready for JUMPING JACKS
+GO!
+..........DONE
+
+All done! Congratulations.
+```
+
+
+# Attribution
+
+Thanks to the Ubuntu project for the sounds.
---
+++ workout.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+WORK = 10
+REST = 5
+AUDIO_PLAYER = 'mpv'
+
+def play file
+	fork {`nohup #{AUDIO_PLAYER} #{file} 2> /dev/null 1> /dev/null`}
+end
+
+workout = File.open('workout.txt').readlines.map(&:chomp)
+
+puts "#{WORK} seconds of work"
+puts "#{REST} seconds of rest"
+total_seconds = (WORK * workout.length) + (REST * (workout.length - 1))
+
+puts "The workout will last #{sprintf("%.2f", total_seconds/60.to_f)} minutes"
+puts
+
+workout.each.with_index do |activity, index|
+	puts "Get ready for #{activity}"
+
+	if index > 0
+		play "./bliss.ogg"
+	end
+
+	sleep REST
+	puts "GO!"
+	play "./system-ready.ogg"
+
+	(WORK - 3).times {print "."; sleep 1}
+
+	3.times do
+		print "."
+		play "./dialog-information.ogg"
+		sleep 1
+	end
+
+	puts "DONE"
+end
+
+play "./harmonics.ogg"
+puts "All done! Congratulations."
