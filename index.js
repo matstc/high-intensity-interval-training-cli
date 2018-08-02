f0c92af1c8032a3d42d373f31ce59cdff70061ef
Matt Tanguay-Carel <matthieutc@gmail.com>
2018-08-02 13:24:34 +0200

    makes simple js app

---
+++ index.js
@@ -0,0 +1,72 @@
+import sampleSize from './node_modules/lodash-es/sampleSize.js'
+
+const WORK = 30
+const REST = 8
+const SAMPLE_SIZE = 10
+
+let startTime = null
+let index = 0
+let saidGo = false
+
+function reset(){
+	index++
+	startTime = null
+	saidGo = false
+}
+
+function prepareFor(activity){
+	let shorthand = activity.replace(/\(.*/g,'')
+	let readable = "Prepare for " + shorthand.toUpperCase()
+	document.querySelector('#PA').textContent = readable
+	speechSynthesis.speak(new SpeechSynthesisUtterance(readable))
+}
+
+function goFor(activity){
+	speechSynthesis.speak(new SpeechSynthesisUtterance("Go!"))
+	let shorthand = activity.replace(/\(.*/g,'')
+	document.querySelector('#PA').textContent = `Go for ${shorthand.toUpperCase()}`
+}
+
+function end(){
+	document.querySelector('#PA').textContent = `DONE!`
+	speechSynthesis.speak(new SpeechSynthesisUtterance("You're done. Congratulations!"))
+	document.querySelector('#stopwatch').innerHTML = "<button onclick='location.reload()'>Go again</button>"
+}
+
+function perform(activities){
+	if (index === activities.length){ return end() }
+
+  const	firstRun = startTime === null && index === 0
+
+	if (startTime === null) {
+		startTime = new Date()
+		prepareFor(activities[index]) 
+	}
+
+	let secondsSinceStart = Math.floor((new Date() - startTime) / 1000)
+	let secondsRemaining = (WORK + REST) - secondsSinceStart
+
+	document.querySelector('#stopwatch').textContent = secondsRemaining
+
+	if (secondsRemaining <= WORK && !saidGo) {
+		goFor(activities[index])
+		saidGo = true
+	}
+
+	if (secondsRemaining < 1){ reset() }
+
+	requestAnimationFrame(perform.bind(null, activities))
+}
+
+export default function main(){
+	document.addEventListener('DOMContentLoaded', function(){
+		fetch('/workout.txt').then(response => response.text()).then(function(data){
+			let all = data.split('\n').filter(s => s.trim() !== '')
+			let activities = sampleSize(all.filter(s => s[0] !== '#'), SAMPLE_SIZE)
+
+			document.querySelector('#setlist').innerHTML = '<ol>' + activities.map(a => `<li>${a}</li>`).join('') + '</ol>'
+
+		  perform(activities)
+		})
+	})
+}
